3ce72905b8c1a91da945c8fdc1a88288079c6d51
eswenke <swenke.ethan.us@gmail.com>
2024-01-12 21:38:14 -0800

    finished form

--- packages/react-frontend/src/MyApp.js
+++ packages/react-frontend/src/MyApp.js
@@ -2,9 +2,8 @@ import React, { useState } from "react";
 import Table from "./Table";
 import Form from "./Form";
 
-
 function MyApp() {
-    const [characters, setCharacters] = useState([]);
+  const [characters, setCharacters] = useState([]);
 
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
@@ -13,14 +12,15 @@ function MyApp() {
     setCharacters(updated);
   }
 
+  function updateList(person) {
+    setCharacters([...characters, person]);
+  }
+
   return (
     <div className="container">
-    <Table
-      characterData={characters}
-      removeCharacter={removeOneCharacter}
-    />
-    <Form />
-  </div>
+      <Table characterData={characters} removeCharacter={removeOneCharacter} />
+      <Form handleSubmit={updateList} />
+    </div>
   );
 }
 
