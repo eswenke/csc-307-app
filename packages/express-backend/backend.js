0a6004d6e38f8da533823911cb5d2b8704c8c3fa
eswenke <swenke.ethan.us@gmail.com>
2024-01-31 20:03:52 -0800

    make POST req for users

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -64,9 +64,20 @@ app.get("/users/:id", (req, res) => {
   }
 });
 
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
+});
+
 const findUserByName = (name) => {
   return users["users_list"].filter((user) => user["name"] === name);
 };
 
 const findUserById = (id) =>
   users["users_list"].find((user) => user["id"] === id);
+
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
